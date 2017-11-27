6ff262835738de448e465fa55647948aeb64803f
ibrahim <sarsouri@post.bgu.ac.il>
2017-11-27 17:57:15 +0200

    hhhh

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -16,20 +14,20 @@ import static java.lang.System.out;
 
 //import java.
 public class ReadFile {
-    Dictionary <String,Document> documents;
+    Map <String,Document> documents ;
     String mainPath;
     List<String> filesPaths;
-    List<String> allMatchesofdocno = new ArrayList<String>();
-    List<String> allMatchesoftext = new ArrayList<String>();
+    List<String> allMatchesofdoc;
     int sizofmydictionary;
-    //List<String> allMatchesofheader = new ArrayList<String>();
+    int nextFile;
 
     public ReadFile(String path)
     {
-  //      documents=new  <String,Document>();
+
         this.mainPath = path;
         this.filesPaths = new ArrayList<String>();
         this.sizofmydictionary=0;
+        this.nextFile=0;
 
         try (Stream<Path> paths = Files.walk(Paths.get(path))) {
             paths.filter(Files::isRegularFile)
@@ -38,16 +36,15 @@ public class ReadFile {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        kfdjkd();
 
     }
 
-    private void kfdjkd()
+    public void breakToFiles()
     {
         List<String> mydocuments;
 
-        mydocuments = new ArrayList<String>();
-        this.filesPaths.forEach(s -> {
+
+        /**this.filesPaths.forEach(s -> {
             try {
                 //out.println(readFileAsString(s));
                 mydocuments.add(readFileAsString(s));
@@ -55,45 +52,58 @@ public class ReadFile {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        });
-        //ttt(mydocuments.get(0));
+        });*/
+        mydocuments = new ArrayList<String>();
+        documents = new HashMap<>();
+        sizofmydictionary=documents.size();
+        allMatchesofdoc = new ArrayList<String>();
+        for (int i = nextFile;i <this.filesPaths.size()&& i < nextFile+10 ;i++)
+        {
+            try {
+                mydocuments.add(readFileAsString(this.filesPaths.get(i)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        nextFile=nextFile+10;
         mydocuments.forEach(s -> {
-            //try {
-                //out.println(readFileAsString(s));
-                ttt(s);
-                //out.println(documents[0]);
-            //} catch (IOException e) {
-                //e.printStackTrace();
-            //}
+            breakToDocs(s);
+
         });
-        //out.println(documents.get(0));
    }
 
-   private  void ttt(String stringfile)
+   private  void breakToDocs(String stringfile)
    {
-       String regex = "<DOCNO>(.+?)</DOCNO>";
-       String regex1 = "<TEXT>(?s)(.+?)</TEXT>";
+       String regex = "<DOC>(?s)(.+?)</DOC>";
+       //String regex1 = "<TEXT>(?s)(.+?)</TEXT>";
        Matcher m = Pattern.compile(regex)
            .matcher(stringfile);
        while (m.find()) {
-           allMatchesofdocno.add(m.group(1));
+           allMatchesofdoc.add(m.group(1));
        }
 
-       Matcher m1 = Pattern.compile(regex1)
-               .matcher(stringfile);
-       while (m1.find()) {
-           allMatchesoftext.add(m1.group(1));
-       }
-       //System.out.println();
-       for(int i=sizofmydictionary;i<allMatchesofdocno.size();i++)
+       for(int i=sizofmydictionary;i<allMatchesofdoc.size();i++)
        {
 
-         Document S = new Document(allMatchesoftext.get(i),0,allMatchesoftext.get(i).length(),"ldkjf");
-           documents.put(allMatchesofdocno.get(i),S);
+
+           if (allMatchesofdoc.get(i).contains("<TEXT>")){
+               int first = allMatchesofdoc.get(i).indexOf("<TEXT>");
+               int last = allMatchesofdoc.get(i).indexOf("</TEXT>");
+               String mytext = allMatchesofdoc.get(i).substring(first+6,last);
+               if(mytext.length()>0) {
+                   first = allMatchesofdoc.get(i).indexOf("<DOCNO>");
+                   last = allMatchesofdoc.get(i).indexOf("</DOCNO>");
+                   String mydocno = allMatchesofdoc.get(i).substring(first+7,last);
+                   Document S = new Document(mytext, 0, mytext.length(), "ldkjf");
+                   documents.put(mydocno, S);
+                   //System.out.println(mydocno);
+               }
 
        }
        sizofmydictionary=documents.size();
-   }
+   }}
     private String readFileAsString(String filePath) throws IOException {
         StringBuffer fileData = new StringBuffer();
         BufferedReader reader = new BufferedReader(
