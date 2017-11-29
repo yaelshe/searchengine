96e83f0a0e1a68a9905fab70ddc89420d08038f1
yaels <yaelsheinberg@gmail.com>
2017-11-29 11:54:01 +0200

    the basic start from hehe

--- src/Parse.java
+++ src/Parse.java
@@ -37,34 +37,53 @@ public class Parse
     }
     private void parseDoc(Document doc)
     {
+        String []termsDoc=doc.getText().split("\\s+");
+        //here we need to put a loop that goes over all the terms on the text
+        //the terms are in "termsDoc"
+
+
 
     }
-    private List<String> breakTextToString (String text)
+    /**private List<String> breakTextToString (String text)
     {
 
         List<String> termsDoc =new ArrayList<String>();
         return termsDoc;
-    }
+    }*/
 
 
-    private boolean isNumber(){return false;}
-private boolean isDate(){return false;}
+    private boolean isNumber(){
+        // a function to check if the term is a number
+        return false;}
+
+private boolean isDate(){
+    // a function to check if the term is part of a date
+        return false;}
 private boolean isCapital()
-{return false;}
+{// // a function to check if the term has capital letter
+    return false;}
 
     private void numbersHandler(String s){
+    //change numer from 83.333333 to 83.33
 
     }
     public static void dateHandler(){
+        //change the format of the date in the text to the rule we have
 
 
     }
     /**private void capitalHandler(String term){
+     * //change the format of the term in the text to the rule we have about capital
+     * // like if Adam Tal need to have 3 terms -> "adam" , "tal" and "adam tal"
+     * also NBA-> nba
+     * also if roGer-> roger
         if (Character.isUpperCase(term.charAt(0)))
         {
             m_terms.
         }
     }*/
+
+    //we need to add 2 more rules like handling word with ' like aren't-> arent or something
 
 
 }
