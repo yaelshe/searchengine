650dbbedd59e0b071a7b36ec8d442300bc84a92d
ibrahim <sarsouri@post.bgu.ac.il>
2017-12-06 11:27:35 +0200

    hhhh

--- src/Indexer.java
+++ src/Indexer.java
@@ -9,4 +9,23 @@ public class Indexer
     public Indexer(Map<String,Term> parsedWords) {
         mp_terms=new HashMap<>(parsedWords);
     }
+    public void WriteToTxt(String s){
+        String string =s;
+        try {
+            //create a temporary file
+            //File file = new File("newfile.txt");
+            File logFile=new File("C:\\Users\\ibrahim\\Desktop\\11\\ibr.txt");
+            System.out.println(logFile.getCanonicalPath());
+
+            //System.out.println(timeLog);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(logFile));
+            writer.write (string);
+            //writer.write("jalksjgghioqwuiplifjkl");
+
+            //Close writer
+            writer.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
