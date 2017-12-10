ce424bec092d87131ff12e30edbe4ad2e9209bc3
yaels <yaelsheinberg@gmail.com>
2017-12-10 11:03:28 +0200

    the basic start from hehe

---
+++ src/IRproject/CacheTermGui.java
@@ -0,0 +1,17 @@
+package IRproject;
+
+public class CacheTermGui {
+    String term;
+
+    public CacheTermGui(String term) {
+        this.term = term;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+}
